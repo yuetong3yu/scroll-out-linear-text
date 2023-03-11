f1cb8c1d9de72a5b63a6396819bf96249a31e889
yuetong3yu <yuetong3yu@gmail.com>
2023-03-11 15:09:51 +1100

    add js.

---
+++ index.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1')
+
+document.addEventListener('scroll', (e) => {
+  let scrolled =
+    document.documentElement.scrollTop /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight)
+
+  h1.style.setProperty('--percentage', `${scrolled * 100}%`)
+})
